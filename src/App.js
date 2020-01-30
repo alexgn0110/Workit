7a5d8e0d0eae7e8c3a78bcb8907768b18731ae22
alexgn0110 <alex_guno0110@hotmail.com>
2020-01-30 11:24:24 -0600

    master

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
   }
       //My APIKey> 2499739844034c77af4097940c655704
       //Workit API> 803fdd9b8517490d89d8c85ade466b8d
+      
   componentDidMount() {
     axios.get(
       'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=803fdd9b8517490d89d8c85ade466b8d'
